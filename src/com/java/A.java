419c2c4b30948989c1d1def5df9acebd41c67069
Preethvasanth <160133655+Preethvasanth@users.noreply.github.com>
2024-02-19 11:59:19 +0530

    Initial commit- I have updated some reverse string code
    Dear vasanthivasanthi
    
    I have updated some code to the reverse string program. It's perfect   then please pull the code and committed.

--- src/com/java/A.java
+++ src/com/java/A.java
@@ -3,6 +3,10 @@ package com.java;
 public class A {
 	public static void main(String[] args)
 	{
-		System.out.println("hello");
+		String str="vasanthakumari"
+			StringBuffer sb=new StringBuffer();
+		sb.append(str);
+		
+		System.out.println(sb.reverse());
 	}
 }
